c6e3bcb1f44767d37e50023e3fc7ae38e9d83a12
Naguissa <naguissa@foroelectro.net>
2024-09-13 13:38:07 +0200

    Add Attiny support and remove uTimerLib dependency

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ Change MESSAGE_TO_SEND define to set the message (keystrokes)
 Change TIME_BETWEEN define in order to select seconds between repetitions.
 
 Dependencies:
- - uTimerLib - https://github.com/Naguissa/uTimerLib - Available on Library Manager
  - Keyboard - https://www.arduino.cc/reference/en/language/functions/usb/keyboard/ - Available on Arduino core libraries
+    - or -
+ - DigiKeyboard - https://github.com/digistump/DigistumpArduino - Available on Arduino after installing Digistump Attiny core
+
 
 Directly compatible boards (others may need extra hardware):
  - Arduino Micro
@@ -30,10 +32,11 @@ Directly compatible boards (others may need extra hardware):
  - Arduino Zero
  - Arduino UNO R4 Minima
  - Arduino UNO R4 WiFi
+ - Digispark Attiny85 compatible boards
 
 From keyboard library: This library is compatible with the avr, samd, sam, renesas_uno architectures.
 
 Creator: Naguissa - https://www.foroelectro.net - https://github.com/Naguissa
 
-Version: 1.0.0
+Version: 2.0.0
 
--- timedKeyboard.ino
+++ timedKeyboard.ino
@@ -4,8 +4,10 @@
  * Change TIME_BETWEEN define in order to select seconds between repetitions.
  *
  * Dependencies:
- *  - uTimerLib - https://github.com/Naguissa/uTimerLib - Available on Library Manager
  *  - Keyboard - https://www.arduino.cc/reference/en/language/functions/usb/keyboard/ - Available on Arduino core libraries
+ *       - or -
+ *  - DigiKeyboard - https://github.com/digistump/DigistumpArduino - Available on Arduino after installing Digistump Attiny core
+ *
  *
  * Directly compatible boards (others may need extra hardware):
  *  - Arduino Micro
@@ -28,34 +30,45 @@
  *  - Arduino Zero
  *  - Arduino UNO R4 Minima
  *  - Arduino UNO R4 WiFi
+ *  - Digispark Attiny85 compatible boards (with DigiStump core)
  *
  * From keyboard library: This library is compatible with the avr, samd, sam, renesas_uno architectures.
  *
  * Creator: Naguissa - https://www.foroelectro.net - https://github.com/Naguissa
  *
- * Version: 1.0.0
+ * Version: 2.0.0
  */
-#include "Keyboard.h"
-#include "uTimerLib.h"
 
 #define MESSAGE_TO_SEND " "
 #define TIME_BETWEEN 10
 
-void setup() {
-  // open the serial port:
-  Serial.begin(9600);
 
-  enableStrokes();
+
+#if defined(ARDUINO_ARCH_AVR) && (defined(ARDUINO_attiny) || defined(ARDUINO_AVR_DIGISPARK) || defined(ARDUINO_AVR_ATTINYX4) || defined(ARDUINO_AVR_ATTINYX5) || defined(ARDUINO_AVR_ATTINYX7) || defined(ARDUINO_AVR_ATTINYX8) || defined(ARDUINO_AVR_ATTINYX61) || defined(ARDUINO_AVR_ATTINY43) || defined(ARDUINO_AVR_ATTINY828) || defined(ARDUINO_AVR_ATTINY1634) || defined(ARDUINO_AVR_ATTINYX313))
+    #define ATTINY 1
+#endif
+
+ 
+#ifdef ATTINY
+    #include "DigiKeyboard.h"
+#else
+    #include "Keyboard.h"
+#endif
+
+void setup() {
+    #ifndef ATTINY
+        Serial.begin(9600); // open the serial port
+    #endif
   }
 
-void sendKeystrokeCb() {
-    Keyboard.print(MESSAGE_TO_SEND);
-}
-
-void enableStrokes() {
-  Keyboard.begin();
-	TimerLib.setInterval_s(sendKeystrokeCb, TIME_BETWEEN);
-}
-
 void loop() {
+    #ifdef ATTINY
+        // DigiKeyboard.sendKeyStroke(0); // Add if problems in old systems
+        DigiKeyboard.print(MESSAGE_TO_SEND);
+        DigiKeyboard.delay(TIME_BETWEEN * 1000);
+    #else
+        Keyboard.print(MESSAGE_TO_SEND);
+        delay(TIME_BETWEEN * 1000);
+    #endif
 }
+
